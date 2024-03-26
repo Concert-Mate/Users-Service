f831c9ec560d4ef6e69a7ef74173b5333c55b13b
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-03-26 20:08:00 +0700

    Initial commit

---
+++ src/main/java/ru/nsu/concertsmate/backend/HelloController.java
@@ -0,0 +1,13 @@
+package ru.nsu.concertsmate.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
