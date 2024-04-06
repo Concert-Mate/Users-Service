1d3372290d018bbd56e3fe88c4f867581c2fe8f4
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-04-06 19:30:52 +0700

    Всё же .env может лежать не в `resources` :D

--- src/main/java/ru/nsu/concertsmate/backend/services/impl/CityServiceImpl.java
+++ src/main/java/ru/nsu/concertsmate/backend/services/impl/CityServiceImpl.java
@@ -2,6 +2,7 @@ package ru.nsu.concertsmate.backend.services.impl;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.elasticsearch.core.search.Hit;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
@@ -20,66 +21,56 @@ import ru.nsu.concertsmate.backend.ElasticCity;
 import ru.nsu.concertsmate.backend.services.CityService;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @Component
 public class CityServiceImpl implements CityService {
-
     @Value("${spring.application.elasticLogin}")
     private String elasticLogin;
 
     @Value("${spring.application.elasticPassword}")
     private String elasticPassword;
 
-
     @Value("${spring.application.elasticHost}")
     private String elasticHost;
-
 
     @Value("${spring.application.elasticPort}")
     private int elasticPort;
 
-
     private ElasticsearchClient elasticsearchClient;
-
 
     private boolean init = false;
 
-
-    private void createElasticsearchClient(){
+    private void createElasticsearchClient() {
         final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(elasticLogin, elasticPassword));
 
-        RestClientBuilder builder = RestClient.builder( new HttpHost(elasticHost, elasticPort))
+        final RestClientBuilder builder = RestClient.builder(new HttpHost(elasticHost, elasticPort))
                 .setHttpClientConfigCallback(httpClientBuilder -> {
                     httpClientBuilder.disableAuthCaching();
-                    return httpClientBuilder
-                            .setDefaultCredentialsProvider(credentialsProvider);
+                    return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
                 });
 
-        ElasticsearchTransport transport = new RestClientTransport( builder.build(), new JacksonJsonpMapper());
+        final ElasticsearchTransport transport = new RestClientTransport(builder.build(), new JacksonJsonpMapper());
 
         elasticsearchClient = new ElasticsearchClient(transport);
     }
 
     @Autowired
-    public CityServiceImpl(){
+    public CityServiceImpl() {
 
     }
 
-
     @Override
     public List<ElasticCity> getCityByName(String name) {
-
-        if (!init){
+        if (!init) {
             createElasticsearchClient();
             init = true;
         }
 
         try {
-            SearchResponse<ElasticCity> response = elasticsearchClient.search(s -> s
+            final SearchResponse<ElasticCity> response = elasticsearchClient.search(s -> s
                             .index("russian_cities")
                             .query(q -> q
                                     .fuzzy(t -> t
@@ -90,13 +81,10 @@ public class CityServiceImpl implements CityService {
                             ),
                     ElasticCity.class
             );
-            List<ElasticCity> res = new ArrayList<>();
-            for (var hit: response.hits().hits()){
-                res.add(hit.source());
-            }
-            return res;
+
+            return response.hits().hits().stream().map(Hit::source).toList();
         } catch (IOException e) {
-            throw new RuntimeException(e); //add unique exception
+            throw new RuntimeException(e); // TODO: add unique exception
         }
     }
 }
