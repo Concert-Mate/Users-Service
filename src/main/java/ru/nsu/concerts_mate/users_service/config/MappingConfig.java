d88089294a2a0a32b99d31df6ffd3498513070fe
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-04-19 14:46:50 +0700

    Добавил автоматические создание очереди при запуске бэкенда. Ранее очередь создавалась лишь при отправке туда первого сообщения, из-за этого бот бы не имел возможности начать слушать эту очередь (потому что до отправки первого сообщения в неё, очередь ещё не существовала)

--- src/main/java/ru/nsu/concerts_mate/users_service/config/MappingConfig.java
+++ src/main/java/ru/nsu/concerts_mate/users_service/config/MappingConfig.java
@@ -2,14 +2,13 @@ package ru.nsu.concerts_mate.users_service.config;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
 
 @Configuration
-public class ApplicationConfig {
+public class MappingConfig {
     @Bean
     public ModelMapper modelMapper() {
         final ModelMapper mapper = new ModelMapper();
@@ -19,10 +18,5 @@ public class ApplicationConfig {
                 .setSkipNullEnabled(true)
                 .setFieldAccessLevel(PRIVATE);
         return mapper;
-    }
-
-    @Bean
-    public Queue queue() {
-        return new Queue("new-concerts-queue");
     }
 }
