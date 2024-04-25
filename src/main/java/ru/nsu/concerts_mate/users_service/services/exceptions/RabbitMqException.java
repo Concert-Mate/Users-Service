922688c140495960edebce46ab3c23aacf9e7e62
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-04-25 11:35:31 +0700

    Пофиксил хуйню

--- src/main/java/ru/nsu/concerts_mate/users_service/services/exceptions/RabbitMqException.java
+++ src/main/java/ru/nsu/concerts_mate/users_service/services/exceptions/RabbitMqException.java
@@ -1,4 +1,7 @@
 package ru.nsu.concerts_mate.users_service.services.exceptions;
 
 public class RabbitMqException extends Exception {
+    public RabbitMqException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
--- src/main/java/ru/nsu/concerts_mate/users_service/services/impl/RabbitMqServiceImpl.java
+++ src/main/java/ru/nsu/concerts_mate/users_service/services/impl/RabbitMqServiceImpl.java
@@ -1,13 +1,13 @@
 package ru.nsu.concerts_mate.users_service.services.impl;
 
+import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.nsu.concerts_mate.users_service.services.RabbitMqEvent;
 import ru.nsu.concerts_mate.users_service.services.RabbitMqService;
-
-import java.awt.image.RasterFormatException;
+import ru.nsu.concerts_mate.users_service.services.exceptions.RabbitMqException;
 
 @Service
 public class RabbitMqServiceImpl implements RabbitMqService {
@@ -21,7 +21,11 @@ public class RabbitMqServiceImpl implements RabbitMqService {
     }
 
     @Override
-    public void sendEvent(RabbitMqEvent event) throws RasterFormatException {
-        template.convertAndSend(queue.getName(), event);
+    public void sendEvent(RabbitMqEvent event) throws RabbitMqException {
+        try {
+            template.convertAndSend(queue.getName(), event);
+        } catch (AmqpException e) {
+            throw new RabbitMqException("Sending event failed", e);
+        }
     }
 }
