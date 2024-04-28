4ebea5802bc668bcf85848506bbf1bd975556a4a
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-04-28 12:53:33 +0700

    Удалил ограничения на сериализацию дат. Теперь даты пишутся в полном формате

--- src/main/java/ru/nsu/concerts_mate/users_service/model/dto/ConcertDto.java
+++ src/main/java/ru/nsu/concerts_mate/users_service/model/dto/ConcertDto.java
@@ -1,6 +1,5 @@
 package ru.nsu.concerts_mate.users_service.model.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,7 +21,6 @@ public class ConcertDto {
 
     private String address;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     private Date datetime;
 
     @JsonProperty(value = "map_url")
